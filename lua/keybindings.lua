e54f24093edb04a479a13a1a0a69bb051ba389b6
Allen Yi <540809998@qq.com>
2023-10-12 19:36:05 +0800

    添加大括号补全功能

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,10 +1,12 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "local map = vim.api.nvim_set_keymap
 
--- Map jj to Esc
+-- jj 映射到 esc
 map("i", "jj", "<Esc>", { noremap = true })
 
--- Map j to gj and k to gk
+-- 视觉行的上下移动
 map("n", "j", 'gj', {})
 map("n", "k", 'gk', {})
 
+-- 自动补全大括号
+map("i", "{", "{<CR>}<Esc>O", {})
