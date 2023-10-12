73f4f31e1cfb89057f9cc7dc20633c442ed559ed
Allen Yi <540809998@qq.com>
2023-10-12 19:45:40 +0800

    更新大括号补全，只在CPP中生效

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "local map = vim.api.nvim_set_keymap
+vim.g.maplocalleader = " "
+local map = vim.api.nvim_set_keymap
 
 -- jj 映射到 esc
 map("i", "jj", "<Esc>", { noremap = true })
@@ -9,4 +10,11 @@ map("n", "j", 'gj', {})
 map("n", "k", 'gk', {})
 
 -- 自动补全大括号
-map("i", "{", "{<CR>}<Esc>O", {})
+-- map("i", "{", "{<CR>}<Esc>O", {})
+-- 在 C++ 文件中按下 '{' 自动补全右大括号
+vim.cmd([[
+augroup CppAutoCloseBrace
+    autocmd!
+    autocmd FileType cpp inoremap <buffer> { {<CR>}<esc>k$a
+augroup END
+]])
